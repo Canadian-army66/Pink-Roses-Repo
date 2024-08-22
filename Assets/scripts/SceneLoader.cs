773b9d92717342c0f163c96e3801d4c9505a2fe1
I Eat Babies <christiankohnen50@gmail.com>
2024-08-22 10:22:29 -0500

    little changes
    make sure to add a script that changes the scene to grandpa's house while keeping the data from the other place

--- Assets/scripts/SceneLoader.cs
+++ Assets/scripts/SceneLoader.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour
 {
     // Start is called before the first frame update
-    public void LoadScene()
+    public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene("Grandpa's room");
     }
