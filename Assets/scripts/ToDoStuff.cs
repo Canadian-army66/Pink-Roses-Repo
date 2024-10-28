9580ce414ff335b34d7423fd3dce2e531e1c1a6b
I Eat Babies <christiankohnen50@gmail.com>
2024-10-28 10:38:12 -0500

    wer

--- Assets/scripts/ToDoStuff.cs
+++ Assets/scripts/ToDoStuff.cs
@@ -18,9 +18,19 @@ public class ToDoStuff : MonoBehaviour
             {
                 mAnimator.SetTrigger("ToKick");
             }
-            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W))
+            if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.W))
             {
                 mAnimator.SetTrigger("ToWalk");
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                mAnimator.SetTrigger("ToWalk");
+                transform.localScale = new Vector3(-6,6,1);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                mAnimator.SetTrigger("ToWalk");
+                transform.localScale = new Vector3(6, 6, 1);
             }
             else if(mAnimator.GetBool("ToIdle") == false)
             {
