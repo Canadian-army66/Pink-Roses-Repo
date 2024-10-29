992448bd5e38a81e9d0cb0d95930391b3c4444df
I Eat Babies <christiankohnen50@gmail.com>
2024-10-29 10:51:52 -0500

    asdesda

---
+++ Assets/scripts/change stance.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changestance : MonoBehaviour
+{
+    private Animator mAnimator;
+    public GameObject dialoguePanel;
+    void Start()
+    {
+        mAnimator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (mAnimator != null)
+        {
+            if (dialoguePanel.activeInHierarchy)
+            {
+                mAnimator.SetBool("isTalking", true);
+            }
+            else
+            {
+                mAnimator.SetBool("isTalking", false);
+            }
+        }
+    }
+}
