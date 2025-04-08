6c8390b5384f696212267dd22f150725383d2088
I Eat Babies <christiankohnen50@gmail.com>
2025-04-08 10:33:33 -0500

    fix state manager

--- Assets/Scrpits/StateManager.cs
+++ Assets/Scrpits/StateManager.cs
@@ -1,27 +1,39 @@
 using JetBrains.Annotations;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class StateManager : MonoBehaviour
 {
-    public bool theyGone = false;
+    public bool theyGone;
     private int enemiesInScene;
     void Start()
     {
         var go = new GameObject { name = "StateManager" };
         DontDestroyOnLoad(go);
         enemiesInScene = GameObject.FindGameObjectsWithTag("Enemy").Length;
-        
+        TurnOff();
+        theyGone = false;
     }
 
     void Update()
     {
+        noEnemies();
+    }
+    public void TurnOff()
+    {
+        if (theyGone == true)
+        {
+            GameObject.FindWithTag("spawnManager").GetComponent<SpawnManager>().enabled = false;
+            Console.WriteLine("Spawner is off");
+        }
+    }
+    public void noEnemies()
+    {
         if (enemiesInScene == 0)
         {
             theyGone = true;
-            GameObject.FindWithTag("spawnManager").GetComponent<SpawnManager>().enabled = false;
         }
     }
-
 }
