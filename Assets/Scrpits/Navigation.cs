fc7e39d3d7c41f8d819fbc68699f3788a4a376bf
I Eat Babies <christiankohnen50@gmail.com>
2025-04-28 10:18:35 -0500

    made navigation work
    last thing is to fix the last scene ;(

--- Assets/Scrpits/Navigation.cs
+++ Assets/Scrpits/Navigation.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Linq;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
-using UnityEngine.InputSystem;
 
-public class Navigator : MonoBehaviour
+public class Navigation : MonoBehaviour
 {
     public Button toGame;
-    void Start()
+    private void Start()
     {
         EventSystem.current.SetSelectedGameObject(toGame.gameObject);
+    }
+    public void SceneNavigator(GameObject ui)
+    {
+        EventSystem.current.SetSelectedGameObject(ui);
     }
 }
